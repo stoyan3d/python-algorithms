2b20c3ff0943a52b2a3705190f4dc80738db5be9
Stoyan Dimitrov <stoyan@spaceapegames.com>
2021-09-22 17:52:37 +0300

    Adding selection sort and some tests for it

--- algorithms/sort.py
+++ algorithms/sort.py
@@ -1,4 +1,8 @@
 def bubble_sort(array: list):
+    """ A sorting algorithm that iterates through an array
+    and swaps two items if the first is smaller than the next.
+    Time complexity: O(N^2)
+    """
     unsorted_until_index = len(array) - 1
     array_sorted = False
 
@@ -10,5 +14,25 @@ def bubble_sort(array: list):
                 array_sorted = False
 
         unsorted_until_index -= 1
+
+    return array
+
+
+def selection_sort(array: list):
+    """
+    Iterate through an array and search for the index of the number
+    with the lowest value. At the end of the iteration swap the first number
+    of the iteration with the lower number. This algorithm is 2x faster than
+    bubble sort even though Big O doesn't reflect it.
+    Time complexity: O(N^2)
+    """
+    for i in range(len(array)):
+        lowest_value_index = i
+        for j in range(i + 1, len(array)):
+            if array[j] < array[lowest_value_index]:
+                lowest_value_index = j
+
+        if lowest_value_index != i:
+            array[i], array[lowest_value_index] = array[lowest_value_index], array[i]
 
     return array
--- tests/test_sort.py
+++ tests/test_sort.py
@@ -1,4 +1,4 @@
-from algorithms.sort import bubble_sort
+from algorithms.sort import bubble_sort, selection_sort
 
 import unittest
 
@@ -20,5 +20,15 @@ class TestSort(unittest.TestCase):
         array_2 = [4, 2, 7, 1, 3]
         self.assertTrue(is_sorted(bubble_sort(array_2)))
 
-        array_3 = [1, 4, 15, 6, 6, 18, 12, 10, 2, 7]
+        array_3 = [18, 5, 15, 6, 6, 16, 12, 10, 2, 7]
         self.assertTrue(is_sorted(bubble_sort(array_3)))
+
+    def test_selection_sort(self):
+        array_1 = [1, 4, 15, 6, 6, 18, 12, 10]
+        self.assertTrue(is_sorted(selection_sort(array_1)))
+
+        array_2 = [4, 2, 7, 1, 3]
+        self.assertTrue(is_sorted(selection_sort(array_2)))
+
+        array_3 = [18, 5, 15, 6, 6, 16, 12, 10, 2, 7]
+        self.assertTrue(is_sorted(selection_sort(array_3)))
