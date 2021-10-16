fc210c2e7e2546eab53f63dbeff790d590c574e9
Stoyan Dimitrov <stoyan3d@gmail.com>
2021-10-17 00:31:14 +0100

    Basic implementation of Doubly Linked List. Needs more testing and optimization

--- datastructures/linkedlist.py
+++ datastructures/linkedlist.py
@@ -1,6 +1,13 @@
-class Node:
+class LinkedListNode:
     def __init__(self, data):
         self.data = data
+        self.next_node = None
+
+
+class DoublyLinkedListNode:
+    def __init__(self, data):
+        self.data = data
+        self.previous_node = None
         self.next_node = None
 
 
@@ -22,7 +29,7 @@ class LinkedList:
         return current_node.data
 
     def insert(self, index: int, value):
-        new_node = Node(value)
+        new_node = LinkedListNode(value)
 
         if index == 0:
             new_node.next_node = self.first_node
@@ -35,15 +42,17 @@ class LinkedList:
                 current_node = current_node.next_node
             else:
                 # We have reached the end of the linked list
+                new_node.previous_node = current_node
                 current_node.next_node = new_node
                 return
 
+        new_node.previous_node = current_node
         new_node.next_node = current_node.next_node
         current_node.next_node = new_node
 
     def append(self, value):
         current_node = self.first_node
-        new_node = Node(value)
+        new_node = LinkedListNode(value)
 
         if not current_node:
             self.first_node = new_node
@@ -84,3 +93,86 @@ class LinkedList:
             current_node.next_node = current_node.next_node.next_node
         else:
             raise IndexError('Linked List index out of range')
+
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.first_node = None
+        self.last_node = None
+
+    def read(self, index: int):
+        if not self.first_node:
+            raise IndexError('Linked List index out of range')
+
+        current_node = self.first_node
+        for i in range(index):
+            if current_node and current_node.next_node:
+                current_node = current_node.next_node
+            else:
+                raise IndexError('Linked List index out of range')
+
+        return current_node.data
+
+    def insert(self, index: int, value):
+        new_node = DoublyLinkedListNode(value)
+
+        if index == 0:
+            new_node.next_node = self.first_node
+            self.first_node = new_node
+            return
+
+        current_node = self.first_node
+        for i in range(index-1):
+            if current_node.next_node:
+                current_node = current_node.next_node
+            else:
+                # We have reached the end of the linked list
+                current_node.next_node = new_node
+                return
+
+        new_node.next_node = current_node.next_node
+        current_node.next_node = new_node
+
+    def append(self, value):
+        new_node = DoublyLinkedListNode(value)
+
+        if not self.first_node:
+            self.first_node, self.last_node = new_node, new_node
+        else:
+            new_node.previous_node = self.last_node
+            self.last_node.next_node = new_node
+            self.last_node = new_node
+
+    def index_of(self, value):
+        index = 0
+        current_node = self.first_node
+        while current_node:
+            if current_node.data == value:
+                return index
+
+            current_node = current_node.next_node
+            index += 1
+
+        return None
+
+    def delete(self, index: int):
+        if not self.first_node:
+            raise IndexError('Linked List index out of range')
+
+        if index == 0:
+            self.first_node = self.first_node.next_node
+            return
+
+        current_node = self.first_node
+        for i in range(index - 1):
+            if current_node:
+                current_node = current_node.next_node
+            else:
+                raise IndexError('Linked List index out of range')
+
+        if current_node.next_node:
+            current_node.next_node.next_node.previous_node = current_node
+            current_node.next_node = current_node.next_node.next_node
+        else:
+            raise IndexError('Linked List index out of range')
+
--- tests/test_linkedlist.py
+++ tests/test_linkedlist.py
@@ -1,5 +1,5 @@
 import unittest
-from datastructures.linkedlist import LinkedList
+from datastructures.linkedlist import LinkedList, DoublyLinkedList
 
 
 class TestLinkedList(unittest.TestCase):
@@ -92,6 +92,113 @@ class TestLinkedList(unittest.TestCase):
 
     def test_delete_out_of_range(self):
         linked_list = LinkedList()
+        with self.assertRaises(IndexError) as c:
+            linked_list.delete(0)
+        self.assertIn('Linked List index out of range', str(c.exception))
+
+        linked_list.append(3)
+        linked_list.append(6)
+        linked_list.append(8)
+
+        with self.assertRaises(IndexError) as c:
+            linked_list.delete(3)
+        self.assertIn('Linked List index out of range', str(c.exception))
+
+        with self.assertRaises(IndexError) as c:
+            linked_list.delete(10)
+        self.assertIn('Linked List index out of range', str(c.exception))
+
+
+class TestDoublyLinkedList(unittest.TestCase):
+    def test_append(self):
+        linked_list = DoublyLinkedList()
+        data = 'something'
+        linked_list.append(data)
+        self.assertEqual(data, linked_list.read(0))
+
+        data = 'else'
+        linked_list.append(data)
+        self.assertEqual(data, linked_list.read(1))
+
+        data = 'happened'
+        linked_list.append(data)
+        self.assertEqual(data, linked_list.read(2))
+
+    def test_read(self):
+        linked_list = DoublyLinkedList()
+        linked_list.append(20)
+        self.assertEqual(20, linked_list.read(0))
+        linked_list.append(10)
+        self.assertEqual(10, linked_list.read(1))
+
+    def test_read_index_out_of_range(self):
+        linked_lists = DoublyLinkedList()
+
+        with self.assertRaises(IndexError) as c:
+            linked_lists.read(0)
+        self.assertIn('Linked List index out of range', str(c.exception))
+
+        linked_lists.append('hello')
+
+        with self.assertRaises(IndexError) as c:
+            linked_lists.read(3)
+        self.assertIn('Linked List index out of range', str(c.exception))
+
+        with self.assertRaises(IndexError) as c:
+            linked_lists.read(1)
+        self.assertIn('Linked List index out of range', str(c.exception))
+
+    def test_insert(self):
+        linked_list = DoublyLinkedList()
+        linked_list.append(3)
+        linked_list.append(6)
+        linked_list.append(8)
+        linked_list.insert(1, 'bee')
+        linked_list.insert(2, 120)
+
+        self.assertEqual('bee', linked_list.read(1))
+        self.assertEqual(120, linked_list.read(2))
+        self.assertEqual(6, linked_list.read(3))
+
+        linked_list.insert(0, 'first')
+        self.assertEqual('first', linked_list.read(0))
+        self.assertEqual(3, linked_list.read(1))
+
+    def test_insert_with_bigger_index(self):
+        linked_list = DoublyLinkedList()
+        linked_list.append(3)
+        linked_list.append(6)
+        linked_list.append(8)
+        linked_list.insert(6, 'test')
+
+        self.assertEqual('test', linked_list.read(3))
+
+    def test_index_of(self):
+        linked_list = DoublyLinkedList()
+        linked_list.append(3)
+        linked_list.append(6)
+        linked_list.append(8)
+
+        self.assertEqual(0, linked_list.index_of(3))
+        self.assertEqual(1, linked_list.index_of(6))
+        self.assertEqual(2, linked_list.index_of(8))
+        self.assertIsNone(linked_list.index_of(10))
+
+    def test_delete(self):
+        linked_list = DoublyLinkedList()
+        linked_list.append(3)
+        linked_list.append(6)
+        linked_list.append(8)
+        linked_list.append(10)
+
+        linked_list.delete(0)
+        self.assertEqual(6, linked_list.read(0))
+
+        linked_list.delete(1)
+        self.assertEqual(10, linked_list.read(1))
+
+    def test_delete_out_of_range(self):
+        linked_list = DoublyLinkedList()
         with self.assertRaises(IndexError) as c:
             linked_list.delete(0)
         self.assertIn('Linked List index out of range', str(c.exception))
