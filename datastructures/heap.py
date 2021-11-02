c6167c2b258a8331cd86862754dca64690f03659
Stoyan Dimitrov <stoyan3d@gmail.com>
2021-11-02 12:00:47 +0000

    Heap implementation but there seems to be a bug in the delete method

---
+++ datastructures/heap.py
@@ -0,0 +1,79 @@
+class Heap:
+    """A heap is a loosly sorted tree structure that is has efficient insertion and largest
+    element deletion. It is the most efficient data structure for Priority Queues.
+    It has similar time efficiency as a Binary Search Tree but we always know what the largest
+    element is."""
+    def __init__(self) -> None:
+        self.data = []
+
+    def insert(self, value: float) -> None:
+        self.data.append(value)
+        self.trickle_up(len(self.data) - 1)
+
+    def trickle_up(self, index: int) -> None:
+        while self.get_parent(index):
+            if self.data[index] > self.get_parent(index):
+                parent_index = self.get_parent_index(index)
+                self.data[index], self.data[parent_index] = self.data[parent_index], self.data[index]
+            index = self.get_parent_index(index)
+
+    def get_parent(self, index: int) -> float:
+        if index <= 0:
+            return None
+
+        parent_index = self.get_parent_index(index)
+        return self.data[parent_index]
+
+    def get_parent_index(self, index: int) -> int:
+        return int((index - 1) / 2)
+
+    def read(self) -> float:
+        return self.data[0]
+
+    def delete(self) -> float:
+        root_node = self.data[0]
+        last_node = self.data.pop()
+        self.data[0] = last_node
+
+        self.trickle_down(0)
+
+        return root_node
+
+    def trickle_down(self, index: int) -> None:
+        while self.get_left(index):
+            min_child_index = self.get_min_child_index(index)
+            if self.data[index] < self.data[min_child_index]:
+                self.data[index], self.data[min_child_index] = self.data[min_child_index], self.data[index]
+
+            index = min_child_index
+
+    def get_min_child_index(self, index: int) -> int:
+        if not self.get_right(index):
+            return self.get_left_index(index)
+        
+        if self.get_left(index) > self.get_right(index):
+            return self.get_right_index(index)
+        else:
+            return self.get_left_index(index)
+
+    def get_left(self, index: int) -> float:
+        left_index = self.get_left_index(index)
+
+        if len(self.data) > left_index:
+            return self.data[left_index]
+        else:
+            return None
+    
+    def get_left_index(self, index: int) -> float:
+        return 2 * index + 1
+
+    def get_right(self, index: int) -> float:
+        right_index = self.get_right_index(index)
+        
+        if len(self.data) > right_index:
+            return self.data[right_index]
+        else:
+            return None
+
+    def get_right_index(self, index: int) -> float:
+        return 2 * index + 2
---
+++ tests/test_heap.py
@@ -0,0 +1,69 @@
+import unittest
+from datastructures.heap import Heap
+
+
+class TestHeap(unittest.TestCase):
+    def test_delete(self):
+        heap = Heap()
+
+        heap.insert(3)
+        heap.insert(25)
+        heap.insert(1)
+        heap.insert(7)
+        heap.insert(2)
+        heap.insert(19)
+        heap.insert(36)
+        heap.insert(100)
+        heap.insert(17)
+
+        self.assertEqual(100, heap.delete())
+        self.assertEqual(36, heap.delete())
+        self.assertEqual(25, heap.delete())
+
+    def test_read(self):
+        heap = Heap()
+
+        heap.insert(3)
+        heap.insert(25)
+        heap.insert(1)
+        self.assertEqual(25, heap.read())
+
+        heap.insert(19)
+        heap.insert(36)
+        self.assertEqual(36, heap.read())
+
+    def test_get_parent(self):
+        heap = Heap()
+
+        heap.insert(3)
+        heap.insert(25)
+        heap.insert(1)
+
+        self.assertEqual(25, heap.get_parent(1))
+
+    def test_get_left(self):
+        heap = Heap()
+
+        heap.insert(3)
+        heap.insert(25)
+
+        self.assertEqual(3, heap.get_left(0))
+
+    def test_get_right(self):
+        heap = Heap()
+
+        heap.insert(3)
+        heap.insert(25)
+        heap.insert(1)
+
+        self.assertEqual(1, heap.get_right(0))
+
+    def test_get_min_child(self):
+        heap = Heap()
+
+        heap.insert(3)
+        heap.insert(25)
+        self.assertEqual(1, heap.get_min_child_index(0))
+
+        heap.insert(1)
+        self.assertEqual(2, heap.get_min_child_index(0))
